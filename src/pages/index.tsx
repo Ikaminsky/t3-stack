b4dc4088522defa7ada763c4a3876aac01fdd8bf
Ilya Kaminskii <ilya.kaminsky@yandex.ru>
2023-04-12 11:57:17 +0300

    Posts router added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "@/utils/api";
-import {SignIn, SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+    const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.post.getAll.useQuery();
   const user = useUser();
+  console.log(data, hello);
 
   return (
     <>
@@ -17,8 +18,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div>
           {user.isSignedIn && <SignOutButton />}
           {!user.isSignedIn && <SignInButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
